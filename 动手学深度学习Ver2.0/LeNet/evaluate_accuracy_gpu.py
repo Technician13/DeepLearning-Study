daf58bf774cace4dcab756563eec770a2262f813
Technician13 <chendonglin_13@163.com>
2021-12-20 13:55:40 +0800

    2021_12_20 完成LeNet

---
+++ 动手学深度学习Ver2.0/LeNet/evaluate_accuracy_gpu.py
@@ -0,0 +1,23 @@
+import torch
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+def evaluate_accuracy_gpu(net, data_iter, device=None): 
+    """使用GPU计算模型在数据集上的精度"""
+    if isinstance(net, nn.Module):
+        net.eval()  # 设置为评估模式
+        if not device:
+            device = next(iter(net.parameters())).device
+    # 正确预测的数量，总预测的数量
+    metric = d2l.Accumulator(2)
+    with torch.no_grad():
+        for X, y in data_iter:
+            if isinstance(X, list):
+                # BERT微调所需的（之后将介绍）
+                X = [x.to(device) for x in X]
+            else:
+                X = X.to(device)
+            y = y.to(device)
+            metric.add(d2l.accuracy(net(X), y), y.numel())
+    return metric[0] / metric[1]
