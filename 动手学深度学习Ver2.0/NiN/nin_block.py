26afa38e1fdc533680cac64a52420b57c0f01cbc
Technician13 <chendonglin_13@163.com>
2021-12-23 10:37:39 +0800

    2021_12_23 完成NiN

---
+++ 动手学深度学习Ver2.0/NiN/nin_block.py
@@ -0,0 +1,9 @@
+import torch
+from torch import nn
+
+def nin_block(in_channels, out_channels, kernel_size, strides, padding):
+    return nn.Sequential(
+        nn.Conv2d(in_channels, out_channels, kernel_size, strides, padding),
+        nn.ReLU(),
+        nn.Conv2d(out_channels, out_channels, kernel_size=1), nn.ReLU(),
+        nn.Conv2d(out_channels, out_channels, kernel_size=1), nn.ReLU())
